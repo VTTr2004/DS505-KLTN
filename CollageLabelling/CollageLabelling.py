f79e5c0dc3d4158df5461d57a959a2649b078140
VTTr2004 <vttr2004@gmail.com>
2025-03-14 09:52:35 +0700

    add test

--- CollageLabelling/CollageLabelling.py
+++ CollageLabelling/CollageLabelling.py
@@ -1,10 +1,20 @@
+import cv2
 
+class Image_Merger:
+    def __init__(self, img_lb, img_bg):
+        # Read image
 
+        self.img_lb = cv2.imread(img_lb)
+        self.img_bg = cv2.imread(img_bg)
 
-#------------------------------------#
+    def Show_lb(self):
+        # Show image label
 
-def CollageLabelling(bg, sign):
-    # Test #
-    print('Chạy thử CollageLabelling')
+        cv2.imshow("Label",self.img_lb)
 
-    return 0
+    def Show_bg(self):
+        # Show image label
+
+        cv2.imshow("Background",elf.img_bg)
+
+print(cv2.__file__)
