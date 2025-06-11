65d61f93311ed041083f1395ad1dd7f85ae93c30
VTTr2004 <vttr2004@gmail.com>
2025-06-11 19:30:12 +0700

    fix drawer

--- worker/drawer.py
+++ worker/drawer.py
@@ -14,15 +14,16 @@ class Draw:
         for id in range(len(chars)):
             char = chars[id]
             if char[-2] == 0:
-                cls_id = char[0]
-                box = self.Convertxyxy(char[4:8])
-
-                name = f'{cls_id}-{id}'
-
                 point_1 = list(map(int,char[4:6]))
                 point_2 = [int(p_1 - p_2) for p_1, p_2 in zip(char[4:6], char[8:10])]
-
-                # cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), color = Draw.color.get(cls_id, (0, 255, 0)), thickness=2)
-                cv2.line(img, point_1, point_2, (255, 255, 255), 3)
-                # cv2.putText(img, name, (box[0], box[1] - 10),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                cv2.line(img, point_1, point_2, (0, 0, 255), 3)
+            if char[-1] != 0:
+                cls_id = char[0]
+                box = self.Convertxyxy(char[4:8])
+                # name = f'{cls_id}-{id}'
+                cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]),
+                              color = Draw.color.get(cls_id, (0, 255, 0)),
+                              thickness=2)
+                # cv2.putText(img, name, (box[0], box[1] - 10),
+                #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         return img
